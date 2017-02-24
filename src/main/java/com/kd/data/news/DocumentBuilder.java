0f317b2eebdbf01b8e42282a09c72a1c76dbeaa0
zhangshuai <shit_qq@163.com>
2017-02-24 15:02:47 +0800

    新文档解析 策略

--- src/main/java/com/kd/data/news/DocumentBuilder.java
+++ src/main/java/com/kd/data/news/DocumentBuilder.java
@@ -16,6 +16,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
+import com.kd.browersearch.domain.BrowserSearchDoc;
 import com.kd.commons.consts.HtmlContentConsts;
 import com.kd.commons.consts.HtmlRegexConsts;
 import com.kd.commons.consts.StringFormatConsts;
@@ -228,6 +229,117 @@ public class DocumentBuilder {
 		return newsDoc;
 	}
 
+	
+	/**
+	 * 默认html 浏览器搜索文档实体
+	 * @param url 
+	 * @param htmlSources
+	 * @return
+	 */
+	public  static BrowserSearchDoc BrowserSearchDocBuild(String url, String htmlSources) {
+		if (StringUtils.isBlank(htmlSources) ) {
+			return null;
+		}
+		BrowserSearchDoc newsDoc = new BrowserSearchDoc();
+		Document doc = Jsoup.parse(htmlSources);
+		// title
+		String title = doc.title();
+		try {
+			if (StringUtils.isBlank(title)) {
+				title = htmlSources.split(HtmlRegexConsts.TITLE_S)[1].split(HtmlRegexConsts.TITLE_E)[0];
+			}
+		} catch (Throwable e) {
+		}
+		
+		// publish datetime
+		String publishTime = doc.body().data().replaceFirst(HtmlRegexConsts.PUBLISH_DATE_TIME_MATCHE, "$1");
+		publishTime = publishTime.matches(HtmlRegexConsts.PUBLISH_DATE_TIME_MATCHE)
+				? publishTime :  doc.body().text().replaceFirst(HtmlRegexConsts.PUBLISH_DATE_TIME_MATCHE, "$1");
+		
+		Map<String, ContentValue> contentMapping = new HashMap<String, ContentValue>();
+		
+		// 猜测正文，p标签统计 start====================>
+		for (Element paperSub : doc.body().select("p").next()) {
+			String key = MD5Util.MD5(paperSub.parent().html());
+			ContentValue value = new ContentValue();
+			if (contentMapping.containsKey(key)) {
+				value = contentMapping.get(key);
+				value.pCount += 1;
+			} else {
+				value.pCount = 1;
+				value.htmlContent = paperSub.parent().text().trim();
+			}
+			contentMapping.put(key, value);
+		}
+		
+		// 正文最大可能:获得最多p标签所在区域 的html文本
+		int maxCount = 0;
+		String maxKey = "";
+		for (Map.Entry<String, ContentValue> entry : contentMapping.entrySet()) {
+			if (null != entry.getValue() && entry.getValue().pCount > maxCount) {
+				maxCount = entry.getValue().pCount;
+				maxKey = entry.getKey();
+			}
+		}
+		
+		StringBuffer contentBuff = new StringBuffer();
+		if (StringUtils.isNotBlank(maxKey)) {
+			logger.info("正文最大概率 区域Key:" + maxKey + ",<p> maxCount:" + maxCount);
+			ContentValue value = contentMapping.get(maxKey);
+			contentBuff.setLength(0);
+			contentBuff.append(value.htmlContent);
+		}
+		/// <<<=========正文统计结束==========
+		
+		// author
+		String author = url.replaceFirst(HtmlRegexConsts.DOMAIN, "$2");
+		
+		// groupId
+		String groupId = DateFormatUtils.format(new Date(), StringFormatConsts.DATE_HOUR_NUMBER_FORMAT);
+		String urlMD5 = MD5Util.MD5(url);
+		
+		// updateTime
+		String updateTime = DateFormatUtils.format(new Date(), StringFormatConsts.SIMPLE_DATETIME_FORMAT);
+		
+		String content = contentBuff.toString();
+		// content.2
+		content = content.split(HtmlContentConsts.CONTENT_END)[0];
+		// content.3
+		content = content.replaceAll(HtmlContentConsts.CONTENT_INNER_MEDIA, "");
+		// content.4
+		content = content.replaceAll(HtmlRegexConsts.EXHANGE_TAG, "");
+		
+		logger.info("result:Start==================<{}>===================", url);
+		logger.info(" url:\t\t {}",url);
+		logger.info(" publishTime:\t\t {}",publishTime);
+		logger.info(" title:\t\t {}",title);
+		logger.info(" author:\t\t {}",author);
+		logger.info(" groupId:\t\t {}",groupId);
+		logger.info(" urlMD5:\t\t {}",urlMD5);
+		logger.info(" updateTime:\t\t {}",updateTime);
+		logger.info(" content:\t\t {}",content.length() > 64 ? content.substring(0, 64) + "......" : content);
+		logger.info("result:End==================================content.length={}", content.length());
+		
+		// 装箱 
+		newsDoc.setAuthor(author);
+		newsDoc.setContent(StringUtils.isBlank(content)?title:content);
+		try {
+			Date publishDate = new SimpleDateFormat("yyyy-MM-dd HH:mm").parse(publishTime);
+			publishTime = DateFormatUtils.format(publishDate, "yyyyMMddHH");
+		} catch (ParseException e) {
+			publishTime = DateFormatUtils.format(new Date(), "yyyyMMddHH");
+		}
+		newsDoc.setDate(Long.valueOf(publishTime));
+		newsDoc.setGroupId(groupId);
+		newsDoc.setId(urlMD5);
+		newsDoc.setTitle(title);
+		newsDoc.setUrl(url);
+		return newsDoc;
+	}
+	
+	
+	
+	
 	public static void main(String[] args) throws Throwable {
 		
 	}
