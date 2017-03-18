3bb209fe6bb7c147e4413df6051c036eb167c355
zhangshuai <shit_qq@163.com>
2017-03-18 15:28:53 +0800

    add

---
+++ src/main/java/com/kd/data/XgWeChatGzhConsumerApp.java
@@ -0,0 +1,40 @@
+package com.kd.data;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import com.kd.data.consumer.NativeConsumer;
+
+
+@SpringBootApplication
+public class XgWeChatGzhConsumerApp {
+
+	@Value("${consume.thread.count}")
+	int consumeAnalyseThreads;
+
+	@Bean
+	public ExecutorService createExcutor(){
+		ExecutorService executor = Executors.newFixedThreadPool(consumeAnalyseThreads);
+		return executor;
+	}
+
+	
+	public static void main(String[] args) throws Exception {
+		final ApplicationContext applicationContext = SpringApplication.run(XgWeChatGzhConsumerApp.class, args);
+		NativeConsumer consumer=applicationContext.getBean(NativeConsumer.class);
+		consumer.init();
+		consumer.topic="xiguaji-topic";
+		Thread.sleep(3000);
+		consumer.consume();
+	}
+	
+}
+
+
+
